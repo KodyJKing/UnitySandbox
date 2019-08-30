c9c11abfcb808ffbf6781355c98403de955c8cf4
KodyJKing <kody.j.king@gmail.com>
2019-08-30 03:43:02 -0700

    init commit

---
+++ Assets/Drag.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class Drag : MonoBehaviour {
+
+    GameObject target;
+    Vector3 localGrabPoint;
+    float grabDistance;
+    Ray mouseRay;
+
+    // Use this for initialization
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Camera cam = GetComponent<Camera>();
+        mouseRay = cam.ScreenPointToRay( Input.mousePosition );
+
+        if ( Input.GetMouseButtonDown( 0 ) ) {
+            RaycastHit hit;
+            if ( Physics.Raycast( mouseRay, out hit ) ) {
+                target = hit.rigidbody.gameObject;
+                localGrabPoint = hit.transform.worldToLocalMatrix.MultiplyPoint( hit.point );
+                grabDistance = hit.distance;
+            }
+        } else if ( Input.GetMouseButtonUp( 0 ) ) {
+            target = null;
+        } else if ( Input.GetMouseButton( 0 ) && target != null ) {
+            Vector3 gp = grabPoint;
+            Vector3 mp = mousePoint;
+            Vector3 diff = mp - gp;
+            Rigidbody body = target.GetComponent<Rigidbody>();
+            body.AddForceAtPosition( diff * 10, gp );
+        }
+    }
+
+    Vector3 grabPoint {
+        get {
+            return target.transform.localToWorldMatrix.MultiplyPoint( localGrabPoint );
+        }
+    }
+
+    Vector3 mousePoint {
+        get {
+            return mouseRay.origin + mouseRay.direction * grabDistance;
+        }
+    }
+
+    void OnDrawGizmos() {
+        if ( target != null )
+            Gizmos.DrawSphere( grabPoint, 0.1f );
+    }
+
+}
